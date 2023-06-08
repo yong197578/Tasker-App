c19847a8238ac6b07b7a9a9621bc928965d595e8
Yong Choi <yong.s.choi2@gmail.com>
2023-06-08 15:41:38 -0500

    initial setup complete

---
+++ src/main/java/com/codeup/tasker/TaskerApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.tasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/tasker/TaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.tasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
